8c2b6405ea33c732de9dce1c6dc73ad1674b6b6d
Jennifer Feng <jjfeng520@gmail.com>
2017-10-31 19:33:19 -0700

    Update assignment

--- Week2/MovieAPIAssignment/MovieAPI/Controllers/HomeController.cs
+++ Week2/MovieAPIAssignment/MovieAPI/Controllers/HomeController.cs
@@ -8,11 +8,49 @@ namespace MovieAPI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DbConnector _dbConnector;
+        public HomeController(DbConnector connect)
+        {
+            _dbConnector = connect;
+        }
+        
         [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
+            string query = $@"SELECT title, release_date, rating, overview FROM movies";
+
+            List<Dictionary<string, object>> allMovies = _dbConnector.Query(query);
+
+            ViewBag.Movies = allMovies;
+            
             return View();
         }
+
+        [HttpPost]
+        [Route("/searchmovie")]
+        public IActionResult SearchMovie(string movieName)
+        {
+            Dictionary<string, dynamic> movieInfo = new Dictionary<string, dynamic>();
+            WebRequest.GetMovieDataAsync(movieName, ApiResponse => {
+                movieInfo = ApiResponse;
+            }).Wait();
+
+            string title = movieInfo["title"];
+            string releaseDate = movieInfo["release"];
+            string overview = movieInfo["overview"];
+            float rating = movieInfo["vote_average"];
+
+            string query = $@"INSERT INTO movies (title, release_date, overview, rating) VALUES ('{title}', '{releaseDate}', '{overview}', '{rating}')";
+
+            _dbConnector.Execute(query);
+
+
+            ViewBag.MovieTitle = title;
+            Console.WriteLine(ViewBag.MovieTitle);
+
+            return RedirectToAction("Index");
+        }
     }
+
 }
---
+++ Week2/MovieAPIAssignment/MovieAPI/Models/Movie.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace MovieAPI
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Overview { get; set; }
+        public float Rating { get; set; }
+    }
+}
